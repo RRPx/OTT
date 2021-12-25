f2fd2e115fa2cbad687013a49c210eaf940ed1c8
Md. Ridwanur <ridwanurrahmanjbwmh@gmail.com>
2021-12-26 05:19:07 +0600

    FirstCommit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const { config } = require('dotenv')
+config()
+const authRoute = require('./routes/auth')
+
+const mongoURL = process.env.MONGO_URL
+const PORT = 4000
+
+mongoose
+  .connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB Connected!')
+  })
+  .catch(() => {
+    console.log('DB Connection Failure!')
+  })
+
+app.use(express.json())
+
+app.use('/server/auth', authRoute)
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`)
+})
---
+++ server/routes/auth.js
@@ -0,0 +1,52 @@
+const { Router } = require('express')
+const app = Router()
+const User = require('../models/User')
+const CryptoJS = require('crypto-js')
+const jwt = require('jsonwebtoken')
+
+// User Registration -> a POST method
+app.post('/register', async ({ body }, res) => {
+  const { username, email, password, age } = body
+  const newUser = new User({
+    username,
+    email,
+    password: CryptoJS.AES.encrypt(password, process.env.SECRET_KEY).toString(),
+    age,
+  })
+
+  try {
+    const user = await newUser.save()
+    res.status(201).json(user)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// LOGIN -> a POST method
+app.post('/login', async ({ body }, res) => {
+  const { email, password } = body
+  try {
+    const user = await User.findOne({ email })
+    if (!user) return res.status(401).json('Wrong Username!')
+    const pass = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY)
+    const originalPassword = pass.toString(CryptoJS.enc.Utf8)
+    if (originalPassword !== password)
+      return res.status(401).json('User is valid but password is wrong!')
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.SECRET_KEY,
+      { expiresIn: '1d' }
+    )
+
+    const { password, ...others } = user._doc
+    res.status(200).json({ ...others, accessToken })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+module.exports = app
